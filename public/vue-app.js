a44944148b740c3be0fa2b9d5511886b6b091a81
code-itaka <code@itaka.one>
2025-06-14 20:02:10 +0300

    addModal added

--- public/vue-app.js
+++ public/vue-app.js
@@ -12,7 +12,28 @@ createApp({
         { title: "Task 3", description: "No phone until 12", reward: "Mental clarity" },
         { title: "Task 3", description: "No phone until 12", reward: "Mental clarity" },
         
-      ]
+      ],
+      showModal: false,
+      newTask: {
+        title: '',
+        description: '',
+        reward: ''
+      }
     };
+  },
+  methods: {
+    openModal(){
+      this.showModal = true;
+    },
+    closeModal(){
+      this.showModal = false;
+      this.newTask = { title: '', description: '', reward: '' };
+    },
+    addTask() {
+      if (this.newTask.title && this.newTask.description && this.newTask.reward) {
+        this.tasks.push({ ...this.newTask });
+        this.closeModal();
+      }
+    }
   }
 }).mount('#app');
